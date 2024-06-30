6880fc1d6a8218185a87db7da5a0385876a32ca4
tsuderia <162472443+tsuderia@users.noreply.github.com>
2024-07-01 03:04:34 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,19 +17,18 @@ const port = process.env.PORT;
 connectDB();
 
 const allowedOrigins = [
-  "https://main.d1sj7cd70hlter.amplifyapp.com",
-  "https://expense-tracker-app-three-beryl.vercel.app",
+  "https://expensetracker-orpin-psi.vercel.app"
   // add more origins as needed
 ];
 
 // Middleware
 app.use(express.json());
 app.use(
-  cors(/*{
+  cors({
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-  }*/
+  }
   )
 );
 app.use(helmet());
