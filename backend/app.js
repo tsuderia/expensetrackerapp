e0fa2a83b660852d5667736d28d3660fa3cf670a
tsuderia <162472443+tsuderia@users.noreply.github.com>
2024-07-01 03:25:40 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,25 +32,25 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(helmet());
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  const origin = req.get('referer');
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
-  // Pass to next layer of middleware
-  if (req.method === 'OPTIONS') res.sendStatus(200);
-  else next();
-});
+// app.use((req, res, next) => {
+//   const origin = req.get('referer');
+//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+//   if (isWhitelisted) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//   }
+//   // Pass to next layer of middleware
+//   if (req.method === 'OPTIONS') res.sendStatus(200);
+//   else next();
+// });
 
 
 // Router
